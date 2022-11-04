1b870b2310b693cb69e6e921d8f6acab3e90a0f4
Anton Orlov <toxa41@list.ru>
2022-11-05 01:46:57 +0300

    Практика методов

---
+++ Exp009_Less/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Задача 32: Напишите программу замена элементов массива:
+//положительные элементы замените на соответствующие отрицательные, и наоборот.
+//[-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+Console.WriteLine("Дан массив: {-4, -8, 8, 2}");
+int [] array = {-4, -8, 8, 2};
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(" "+ -array[i]);
+}
---
+++ Exp011_Method/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 1, 2, 31, 55, 43, 5, 6, 3 };
+
+int n = array.Length;
+int find = 43;
+
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine($"Инекс числа: {index}");
+    }
+        index++;
+}
