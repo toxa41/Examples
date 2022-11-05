eade19aae29f2bd58558459dc7cfe65cc25a6171
Anton Orlov <toxa41@list.ru>
2022-11-05 03:21:57 +0300

    Практика методов, массивов

---
+++ Exp012_Array/Program.cs
@@ -0,0 +1,27 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1,100);
+        index++;
+    }
+
+}
+
+void PrintAray (int [] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(" "+col[position]);
+        position++;
+    }
+}
+
+int [] array = new int [10];
+
+FillArray(array);
+PrintAray(array);
---
+++ Exp013_Mass_min_max/Program.cs
@@ -0,0 +1,34 @@
+﻿
+int[] array = { 1, 5, 7, 9, 1, 3, 5, 6 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+
+        for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+PrintArray(array);
+SelectionSort(array);
+
+
+PrintArray(array);
